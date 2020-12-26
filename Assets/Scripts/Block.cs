ff63e2b706b4fcf2e4438e40f85d4863e2e13c34
Tristan Newball <ninjatrisboy@gmail.com>
2020-12-26 09:49:10 -0600

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    Level level;
+
+
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] AudioClip[] breakSound;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
+
+    [SerializeField] int timesHit;
+
+    private void Start()
+    {
+        CountBreakablBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void CountBreakablBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.AddToBreakableBlocksCount();            
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+
+            DestroyBlock();
+
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+
+
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+
+   
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 16f;
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+
+       
+    }
+
+    private float  GetXPos()
+    {
+        if (FindObjectOfType<GameSession>().IsAutoPlayEnabled())
+        {
+            return FindObjectOfType<Ball>().transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
